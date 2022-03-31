2101141159557b9b2e0598d34a4a0f9d2b79ac4c
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-31 12:16:21 -0700

    refactor

--- reduce.js
+++ reduce.js
@@ -2,11 +2,13 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => brands.reduce((a, b) => [...a, ...b.shoes], '');
+export const getSneaks = (brands) => brands.reduce((a, b) => [...a, ...b.shoes], []);
 // export const getSneaks = (brands) => brands.map((brand) => brand.shoes).flat();
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) =>
-  dogs.map((dog) => dog.age).reduce((a, b) => a + b, 0) / dogs.length;
+// export const getAverageAge = (dogs) =>
+//   dogs.map((dog) => dog.age).reduce((a, b) => a + b, 0) / dogs.length;
+
+export const getAverageAge = (dogs) => dogs.reduce((a, b) => a + b.age, 0) / dogs.length;
